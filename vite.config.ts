0a9058213a504f33ff9d1467cf9e739305d806d8
Akimana A Jean d Amour <akimanaja17@gmail.com>
2025-06-18 10:11:37 +0200

    Add publishable settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,40 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import dts from 'vite-plugin-dts';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts()],
   server: {
     port: 3000,
   },
   build: {
-    outDir: 'build',
-    sourcemap: true,
-  },
-  resolve: {
-    alias: {
-      '@appTypes': path.resolve(__dirname, 'src/appTypes'),
+    lib: {
+      entry: 'src/index.ts',
+      name: 'ReactJsonFormInputs',
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', /^@mui\//, /^@emotion\//],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          '@mui/material': 'MaterialUI',
+          '@emotion/react': 'emotionReact',
+          '@emotion/styled': 'emotionStyled',
+          '@mui/icons-material': 'MaterialUIIcons',
+          '@mui/lab': 'MaterialUILab',
+          '@mui/system': 'MaterialUISystem',
+          '@mui/styles': 'MaterialUIStyles',
+          '@mui/utils': 'MaterialUIUtils',
+          '@mui/base': 'MaterialUIBase',
+          '@mui/x-date-pickers': 'MaterialUIXDatePickers',
+          '@mui/x-date-pickers/AdapterDayjs':
+            'MaterialUIXDatePickersAdapterDayjs',
+          '@mui/x-date-pickers/AdapterDateFns':
+            'MaterialUIXDatePickersAdapterDateFns',
+        },
+      },
     },
   },
 });
